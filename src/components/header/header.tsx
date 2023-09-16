f69527613586f7fab572fba1e16f3f127c206282
Sameer Kumar <kumar.sam1612@gmail.com>
2023-09-17 00:13:23 +0530

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <nav className="navbar bg-info shadow-sm">
       <div className="container-fluid mx-4">
-        <h1 className="navbar-brand text-light mb-0">Tiers of Joy</h1>
+        <h1 className="navbar-brand text-light mb-0">256 Shades of Gray</h1>
         <a
           href="http://github.com/sameer1612/256-shades-of-gray"
           target="_blank"
